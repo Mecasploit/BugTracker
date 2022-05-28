53e06dc5378c9ea9bf7eb634e48504c303540c98
Mecasploit <walidbenhd@gmail.com>
2022-05-28 15:00:21 +0200

    added the creation of tickets fonctionality

--- Bug_Tracker_Web/Migrations/20220525103506_AddDescriptionTickets.Designer.cs
+++ Bug_Tracker_Web/Migrations/20220525103506_AddDescriptionTickets.Designer.cs
@@ -4,6 +4,7 @@ using Bug_Tracker_Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bug_Tracker_Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220525103506_AddDescriptionTickets")]
+    partial class AddDescriptionTickets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
